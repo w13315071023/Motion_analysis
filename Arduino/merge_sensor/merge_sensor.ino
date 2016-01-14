2f3c6edd73cfcfef6e80c909d73717659045ccbf
LincolnBurrows <1049366956@qq.com>
2016-01-14 09:41:20 +0800

    [arduino]create merge project

---
+++ Arduino/merge_sensor/merge_sensor.ino
@@ -0,0 +1,15 @@
+/*
+ * 2016.1.4:
+ * combine sound sensor with laser sensor to one project
+ * detect hit ball motion
+ * send a signal '1' to PC by uart 9600 8N1
+ */
+void setup() {
+
+
+}
+
+void loop() {
+  
+
+}
