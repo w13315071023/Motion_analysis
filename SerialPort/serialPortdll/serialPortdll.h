17ab411d5414056328ccae291fefbb8eb9e7efc8
LincolnBurrows <1049366956@qq.com>
2016-01-20 11:13:38 +0800

    add comments

--- SerialPort/serialPortdll/serialPortdll.h
+++ SerialPort/serialPortdll/serialPortdll.h
@@ -6,7 +6,7 @@
 #define SERIALPORTDLL_API __declspec(dllimport)
 #endif
 
-SERIALPORTDLL_API int get_com_num();
-SERIALPORTDLL_API bool open_com_port(int com_num);
+SERIALPORTDLL_API int get_com_num(); //0:no valid com port
+SERIALPORTDLL_API bool open_com_port(int com_num); //TRUE:open successful; FALSE:open failed
 SERIALPORTDLL_API unsigned int fetch_msg(); 
 //1:no ball; 2:has ball no hit; 3:has ball hit valid; 4:has ball hit invalid;
